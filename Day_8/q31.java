e747425d35749aafaa174b62cb604146b87b6670
j-hajarat-aa <hajaratansari0000@gmail.com>
2026-06-08 17:01:32 +0530

    Day 8 uploaded

---
+++ Day_8/q31.java
@@ -0,0 +1,16 @@
+// package Day_8;
+
+public class q31 {
+    public static void main(String[] args) {
+
+        for (int i = 65; i <= 69; i++) {
+            for (int j = 65; j <= i; j++) {
+                System.out.print((char) j + " ");
+            }
+            System.out.println();
+
+        }
+
+    }
+
+}
