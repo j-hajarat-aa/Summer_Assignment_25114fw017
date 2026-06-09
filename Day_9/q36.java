6dc00ce166f7f34c88aeb20a8e62198e01436513
j-hajarat-aa <hajaratansari0000@gmail.com>
2026-06-09 15:45:31 +0530

    Day 9 uploaded

---
+++ Day_9/q36.java
@@ -0,0 +1,23 @@
+public class q36 {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int row = 1; row <= n; row++) {
+
+            for (int col = 1; col <= n; col++) {
+                if (row == 1 || row == n || col == 1 || col == 5) {
+
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+
+            }
+
+            System.out.println();
+        }
+
+    }
+
+}
